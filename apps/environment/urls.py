4ae2e6b01257b0dc7fe527e1e761b10b833d36f5
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-16 19:09:02 -0300

    feat: CameraEnvironmentCreateView

--- apps/environment/urls.py
+++ apps/environment/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.environment.views import CameraEnvironmentListView
+from apps.environment.views import CameraEnvironmentListView, CameraEnvironmentCreateView
 
 urlpatterns = [
     path("environment/", CameraEnvironmentListView.as_view(), name="environment_list"),
+    path("environment/create-environment/", CameraEnvironmentCreateView.as_view(), name="environment_create"),
 ]
--- apps/environment/views.py
+++ apps/environment/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from apps.environment.models import CameraEnvironment
 
 
@@ -7,3 +7,10 @@ class CameraEnvironmentListView(ListView):
     template_name = "environment/environment.html"
     model = CameraEnvironment
     context_object_name = "camera_environments"
+
+
+class CameraEnvironmentCreateView(CreateView):
+    template_name = "environment/create_environment.html"
+    model = CameraEnvironment
+    fields = "__all__"
+    success_url = reverse_lazy("environment_list")
