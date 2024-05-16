3278e8dbdb3f954d48b43bc10e83dc21432d59dd
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-16 18:03:34 -0300

    feat: models e migrations

--- apps/camera/models.py
+++ apps/camera/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from apps.environment.models import CameraEnvironment
 
-# Create your models here.
+
+class Camera(models.Model):
+    description = models.CharField(max_length=100)
+    ip = models.GenericIPAddressField(protocol="both", unpack_ipv4=True, unique=True)
+    port = models.IntegerField()
+    user = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    display_on_home = models.BooleanField(default=False)
+    camera_environment = models.ForeignKey(
+        CameraEnvironment,
+        on_delete=models.CASCADE,
+        related_name="camera_environment",
+        verbose_name="Camera environment",
+    )
+
+    def __str__(self):
+        return self.description
