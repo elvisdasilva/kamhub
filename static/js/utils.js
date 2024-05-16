e05ab96d7ddc0225bade14513e485c269af791b1
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-16 19:56:19 -0300

    feat: configuração do dataTables

---
+++ static/js/utils.js
@@ -0,0 +1,17 @@
+export function defaultOptions() {
+	let options = {
+		paging: true,
+		lengthChange: true,
+		searching: true,
+		ordering: true,
+		info: true,
+		autoWidth: false,
+		responsive: true,
+		dom: "lBfrtip",
+		language: {
+			url: "//cdn.datatables.net/plug-ins/2.0.0/i18n/pt-BR.json",
+		},
+	};
+
+	return options;
+}
