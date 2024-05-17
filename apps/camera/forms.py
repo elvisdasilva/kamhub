8fe6e5b71c02b99ceeff4cfce6ff374602655f4b
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-17 00:09:08 -0300

    refactor: campos no form

--- apps/camera/forms.py
+++ apps/camera/forms.py
@@ -1,8 +1,24 @@
-from django.forms import ModelForm
+from django import forms
 from apps.camera.models import Camera
 
 
-class CameraForm(ModelForm):
+class CameraForm(forms.ModelForm):
+
     class Meta:
         model = Camera
+        widgets = {
+            "description": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "ex: frente rua lado esquerdo"}
+            ),
+            "ip": forms.TextInput(attrs={"class": "form-control", "placeholder": "ex: 192.168.1.2"}),
+            "port": forms.NumberInput(attrs={"class": "form-control", "placeholder": "ex: 554"}),
+            "user": forms.TextInput(attrs={"class": "form-control", "placeholder": "ex: admin"}),
+            "password": forms.PasswordInput(attrs={"class": "form-control", "placeholder": "ex: 123"}),
+            "display_on_home": forms.CheckboxInput(
+                attrs={"class": "form-check-input custom-control-input"}
+            ),
+            "camera_environment": forms.Select(
+                attrs={"class": "form-select select2bs4 form-control"}
+            ),
+        }
         fields = "__all__"
