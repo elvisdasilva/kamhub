366c13f78627d16756f41283b1f865c47cd53e4a
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-17 00:09:48 -0300

    feat: inicia o select2

--- static/js/modules/camera/camera.js
+++ static/js/modules/camera/camera.js
@@ -1,6 +1,14 @@
 import { defaultOptions } from "../../utils.js";
 
 $(document).ready(function () {
-    let cameraTable = $("#tableCamera");
-    cameraTable.DataTable(defaultOptions());
+	let cameraTable = $("#tableCamera");
+	cameraTable.DataTable(defaultOptions());
+
+	$(".select2bs4").select2({
+		theme: "bootstrap4",
+		placeholder: "Selecione o ambiente",
+		allowClear: true,
+		language: "pt-BR",
+		width: "100%",
+	});
 });
