c7aa54cf67aabae8e0ecdfeb6ba8574c75243148
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-16 22:07:06 -0300

    feat: rota para cadastro de câmera

--- apps/camera/urls.py
+++ apps/camera/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.camera.views import CameraListView
+from apps.camera.views import CameraListView, CameraCreateView
 
 urlpatterns = [
     path("camera/", CameraListView.as_view(), name="camera_list"),
+    path("camera/create-camera/", CameraCreateView.as_view(), name="camera_create"),
 ]
