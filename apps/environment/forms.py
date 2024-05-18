8a1fa89ee2216ac909b8c4336f7dc489bea703d9
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-18 14:18:41 -0300

    refactor: form

--- apps/environment/forms.py
+++ apps/environment/forms.py
@@ -1,8 +1,14 @@
-from django.forms import ModelForm
+from django import forms
 from apps.environment.models import CameraEnvironment
 
 
-class CameraEnvironmentForm(ModelForm):
+class CameraEnvironmentForm(forms.ModelForm):
+
     class Meta:
         model = CameraEnvironment
+        widgets = {
+            "description": forms.TextInput(
+                attrs={"class": "form-control form-control-user", "placeholder": "ex: sala de estar"}
+            )
+        }
         fields = "__all__"
