00501bff4209fc75bbdfae8141abbaaa142df9bf
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-18 16:11:22 -0300

    feat: contexto para listar as câmeras na home

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from apps.camera.models import Camera
 
 
 def home(request):
-    return render(request, "home/home.html")
+    return render(request, "home/home.html", {"cameras": Camera.objects.filter(display_on_home=True)})
