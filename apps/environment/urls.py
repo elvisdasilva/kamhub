04cbe453621acb56b599ffa559cc756201d72288
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-18 14:19:11 -0300

    feat: CameraEnvironmentUpdateView e CameraEnvironmentDeleteView

--- apps/environment/urls.py
+++ apps/environment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.environment.views import CameraEnvironmentListView, CameraEnvironmentCreateView
+from apps.environment.views import CameraEnvironmentListView, CameraEnvironmentCreateView, CameraEnvironmentUpdateView, CameraEnvironmentDeleteView
 
 urlpatterns = [
     path("environment/", CameraEnvironmentListView.as_view(), name="environment_list"),
     path("environment/create-environment/", CameraEnvironmentCreateView.as_view(), name="environment_create"),
+    path("environment/<int:pk>/update-environment/", CameraEnvironmentUpdateView.as_view(), name="environment_update"),
+    path("environment/<int:pk>/delete-environment/", CameraEnvironmentDeleteView.as_view(), name="environment_delete"),
 ]
--- apps/environment/views.py
+++ apps/environment/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from apps.environment.models import CameraEnvironment
 from django.contrib import messages
+from apps.environment.forms import CameraEnvironmentForm
 
 
 class CameraEnvironmentListView(ListView):
@@ -13,7 +14,7 @@ class CameraEnvironmentListView(ListView):
 class CameraEnvironmentCreateView(CreateView):
     template_name = "environment/create_environment.html"
     model = CameraEnvironment
-    fields = "__all__"
+    form_class = CameraEnvironmentForm
     success_url = reverse_lazy("environment_list")
 
     def form_valid(self, form):
@@ -23,3 +24,28 @@ class CameraEnvironmentCreateView(CreateView):
     def form_invalid(self, form):
         messages.error(self.request, "Erro ao cadastrar ambiente.")
         return super().form_invalid(form)
+
+
+class CameraEnvironmentUpdateView(UpdateView):
+    template_name = "environment/update_environment.html"
+    model = CameraEnvironment
+    form_class = CameraEnvironmentForm
+    success_url = reverse_lazy("environment_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Ambiente atualizado com sucesso.")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Erro ao atualizar ambiente.")
+        return super().form_invalid(form)
+
+
+class CameraEnvironmentDeleteView(DeleteView):
+    template_name = "environment/delete_environment.html"
+    model = CameraEnvironment
+    success_url = reverse_lazy("environment_list")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Ambiente deletado com sucesso.")
+        return super().delete(request, *args, **kwargs)
