666eb9861ecb302c35cda5d1ca325ce16bfc9a1e
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-18 14:55:49 -0300

    feat: rotas para update e delete da câmera

--- apps/camera/urls.py
+++ apps/camera/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.camera.views import CameraListView, CameraCreateView
+from apps.camera.views import CameraListView, CameraCreateView, CameraUpdateView, CameraDeleteView
 
 urlpatterns = [
     path("camera/", CameraListView.as_view(), name="camera_list"),
     path("camera/create-camera/", CameraCreateView.as_view(), name="camera_create"),
+    path("camera/<int:pk>/update-camera/", CameraUpdateView.as_view(), name="camera_update"),
+    path("camera/<int:pk>/delete-camera/", CameraDeleteView.as_view(), name="camera_delete"),
 ]
