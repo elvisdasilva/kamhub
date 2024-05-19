c4bfc0dd0f18e0348caf7042f334adad8b21328d
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-19 12:57:59 -0300

    refactor: remove mensagens de erro do js

--- static/js/modules/home/home.js
+++ static/js/modules/home/home.js
@@ -1,50 +1,17 @@
 $(document).ready(function () {
-	$(".view-camera").click(function (e) {
-		e.preventDefault();
-		let streamUrl = $(this).data("stream-url");
-		let errorMessage = $(this).closest(".card").find(".error-message");
-
-		// Clear previous error message
-		errorMessage.hide().text("");
-
-		$.ajax({
-			url: streamUrl,
-			type: "GET",
-			success: function (data) {
-				$("#cameraPreview").attr("src", streamUrl + "?t=" + timestamp);
-				$("#cameraModal").modal("show");
-			},
-			error: function (xhr) {
-				let errorText =
-					xhr.responseJSON && xhr.responseJSON.error
-						? xhr.responseJSON.error
-						: "An error occurred while trying to fetch the camera stream.";
-				errorMessage.text(errorText).show();
-			},
-		});
+	$(".update-camera").click(function () {
+		$(this).prop("disabled", true);
+		const streamUrl = $(this).data("stream-url");
+		const imgId = $(this).closest(".card").find("img").attr("id");
+		$("#" + imgId).attr("src", streamUrl + "?_t=" + Date.now());
+		setTimeout(() => {
+			$(this).prop("disabled", false);
+		}, 1000);
 	});
 
-	$(".update-camera").click(function (e) {
+	$(".view-camera").click(function (e) {
 		e.preventDefault();
-		let streamUrl = $(this).data("stream-url");
-		let errorMessage = $(this).closest(".card").find(".error-message");
-
-		// Clear previous error message
-		errorMessage.hide().text("");
-
-		$.ajax({
-			url: streamUrl,
-			type: "GET",
-			success: function (data) {
-				$("#camera-stream-" + camera.id).attr("src", streamUrl + "?t=" + timestamp);
-			},
-			error: function (xhr) {
-				let errorText =
-					xhr.responseJSON && xhr.responseJSON.error
-						? xhr.responseJSON.error
-						: "An error occurred while trying to update the camera stream.";
-				errorMessage.text(errorText).show();
-			},
-		});
+		var streamUrl = $(this).data("stream-url");
+		$("#cameraPreview").attr("src", streamUrl);
 	});
 });
