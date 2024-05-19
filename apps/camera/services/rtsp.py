d0b9f4079481bf90ecef310f5e41ca8d4fcc83fe
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-19 15:07:06 -0300

    feat: adiciona detecção de massa com yolo

--- apps/camera/services/rtsp.py
+++ apps/camera/services/rtsp.py
@@ -1,13 +1,33 @@
 from django.http import JsonResponse, StreamingHttpResponse
 import cv2
+import os
 
 class StreamRTSP:
 
     @staticmethod
     def gen(user, password, ip, port):
+
+        # class colors
+        COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        coco_names_path = os.path.join(base_path, "coco.names")
+        weights_path = os.path.join(base_path, "yolov4-tiny.weights")
+        config_path = os.path.join(base_path, "yolov4-tiny.cfg")
+
+        class_names = []
+        with open(coco_names_path, "r") as f:
+            class_names = [cname.strip() for cname in f.readlines()]
+
         cap = cv2.VideoCapture(
             f"rtsp://{user}:{password}@{ip}:{port}/cam/realmonitor?channel=1&subtype=0"
         )
+
+        # Load weights and config neural network
+        net = cv2.dnn.readNet(weights_path, config_path)
+
+        # Set params neural network
+        model = cv2.dnn_DetectionModel(net)
+        model.setInputParams(size=(416, 416), scale=1 / 255)
 
         if not cap.isOpened():
             cap.release()
@@ -18,6 +38,22 @@ class StreamRTSP:
             if not success:
                 break
             else:
+                classes, scores, boxes = model.detect(frame, 0.1, 0.3)
+
+                for (classid, score, box) in zip(classes, scores, boxes):
+                    color = COLORS[int(classid) % len(COLORS)]
+                    label = "%s : %f" % (class_names[classid], score)
+                    cv2.rectangle(frame, box, color, 2)
+                    cv2.putText(
+                        frame,
+                        label,
+                        (box[0], box[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        color,
+                        2,
+                    )
+
                 ret, buffer = cv2.imencode(".jpg", frame)
                 frame = buffer.tobytes()
                 yield (
