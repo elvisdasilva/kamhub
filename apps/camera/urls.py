355180c2fee8d2b3fbeb4ddd37efd580bf749d63
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-18 21:42:36 -0300

    feat: rota para StreamView

--- apps/camera/urls.py
+++ apps/camera/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.camera.views import CameraListView, CameraCreateView, CameraUpdateView, CameraDeleteView
+from apps.camera.views import CameraListView, CameraCreateView, CameraUpdateView, CameraDeleteView, StreamView
 
 urlpatterns = [
     path("camera/", CameraListView.as_view(), name="camera_list"),
     path("camera/create-camera/", CameraCreateView.as_view(), name="camera_create"),
     path("camera/<int:pk>/update-camera/", CameraUpdateView.as_view(), name="camera_update"),
     path("camera/<int:pk>/delete-camera/", CameraDeleteView.as_view(), name="camera_delete"),
+    path("home/stream/<str:user>/<str:password>/<str:ip>/<int:port>/", StreamView.as_view(), name="stream"),
 ]
